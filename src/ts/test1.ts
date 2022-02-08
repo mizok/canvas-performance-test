ed5d4a8c312a7e2944cc93e614c8785782b0f882
Mizok <karasunohina@gmail.com>
2022-02-08 22:29:01 +0800

    取消掉IIFE改成用onload

--- src/ts/test1.ts
+++ src/ts/test1.ts
@@ -2,11 +2,11 @@ import {Composition} from 'mkcvs';
 import {A} from './lib/draw';
 
 
-(()=>{
+window.onload = ()=>{
   const compEle = document.getElementById('composition');
   const comp = new Composition(compEle);
   for(let i=0;i<10;i++){
     const layer  = comp.newLayer('2d');
     new A(layer);
   }
-})()
+}
--- src/ts/test2.ts
+++ src/ts/test2.ts
@@ -1,8 +1,8 @@
 import {B} from './lib/draw';
 
-(()=>{
+window.onload = ()=>{
   const compEle = document.getElementById('composition');
   compEle.querySelectorAll('canvas').forEach((o,i)=>{
     new B(o);
   })
-})()
+}
